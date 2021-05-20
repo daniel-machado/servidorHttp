40e6495e4d550fd826785362cd22c3ee91e65487
Daniel Machado <daniel7oficial@gmail.com>
2021-05-20 09:27:04 -0300

    [#2] - Alteração do mocks

--- mocks/users.js
+++ mocks/users.js
@@ -4,15 +4,4 @@ module.exports = [
 {  id: 2, name: 'Messi', },
 {  id: 3, name: 'Alves', },
 {  id: 4, name: 'Marcelo', },
-
-
-
-/*
-{  id: 2, name: 'Ellen', },
-{  id: 3, name: 'Flavia', },
-{  id: 4, name: 'Vitoria', },
-{  id: 5, name: 'Maiara', },
-{  id: 6, name: 'Alessanda', },
-{  id: 7, name: 'Sotonanda', },
-*/
 ];
